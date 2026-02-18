f5d967fc4ea3b6b9669683e9a2c32de1642f8838
M4nu0w2 <emanu.giardino@gmail.com>
2026-02-18 10:22:57 +0100

    project initialize

---
+++ check-room/src/main/java/com/example/check_room/CheckRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.check_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckRoomApplication.class, args);
+	}
+
+}
